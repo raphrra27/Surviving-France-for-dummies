198e6a577aec09427ebac7cb2e72341c260ac449
erwannbr <erwann.briot@efrei.net>
2026-09-25 13:45:09 +0800

    first commit, setup for vue.js and all required ressources

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "your_db_name",
+});
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
---
+++ surviving-france-for-dummies/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>Surviving France for Dummies</h1>
+  </main>
+</template>
